dc1eedd522f6dcd07539cdfb70b5a95deca9188a
sultanby <jemjasott@gmail.com>
2020-12-01 17:56:13 +0100

    last finished hw

---
+++ js-form-homework/dist/script.js
@@ -0,0 +1,14 @@
+/*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+
+function billingFunction() {
+  var ShipName = document.getElementById('shippingName').value;
+  var ShipZip = document.getElementById('shippingZip').value;
+  if (document.getElementById('same').checked){
+   document.getElementById('billingName').value = ShipName;
+   document.getElementById('billingZip').value = ShipZip;
+  }
+  else{
+    document.getElementById('billingName').value = '';
+    document.getElementById('billingZip').value = '';
+  }
+}
